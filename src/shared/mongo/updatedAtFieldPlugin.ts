8469d96a0ca29aece7462d08604d02090095392d
EHalimon <evgeniyhalimon321@gmail.com>
2025-06-11 16:00:33 +0300

    updated script, added new dep

--- src/shared/mongo/updatedAtFieldPlugin.ts
+++ src/shared/mongo/updatedAtFieldPlugin.ts
@@ -1,12 +1,21 @@
-import { Schema, Document } from 'mongoose';
+import { Schema } from 'mongoose';
 
 export function updatedAtFieldPlugin(schema: Schema) {
-  schema.pre(['save', 'findOneAndUpdate'], function (next) {
-    if (this instanceof Document) {
-      this.updatedAt = new Date();
-    } else {
-      this.set({ updatedAt: new Date() });
+  schema.pre('save', function (next) {
+    (this as any).updatedAt = new Date();
+    next();
+  });
+
+  schema.pre('findOneAndUpdate', function (next) {
+    const update = this.getUpdate() as Record<string, any>;
+
+    if (!update.$set) {
+      update.$set = {};
     }
+
+    update.$set.updatedAt = new Date();
+    this.setUpdate(update);
+
     next();
   });
 }
